3c812ed8187f37ecab9a23bd52de9575b86dd281
unknown <srisannidhi.mujumdar@FireEye.com>
2018-06-22 11:47:31 +0000

    Agent modules integration

--- client/pro-malhunt/com/malhunt/agent/backend/main.py
+++ client/pro-malhunt/com/malhunt/agent/backend/main.py
@@ -5,6 +5,7 @@ socketIO = SocketIO('10.33.34.19', 3000)
 
 
 def find_in_system(message):
+	print 'Hello'
 	if type(message) == type('a'):
 		isReg = 'Yes' if query_registry.search_registry(message) else 'No'
 		socketIO.emit('message', '{}: {}'.format(message, isReg))
--- client/pro-malhunt/com/malhunt/agent/backend/query_registry.py
+++ client/pro-malhunt/com/malhunt/agent/backend/query_registry.py
@@ -1,5 +1,6 @@
 import os
 import re
+from com.malhunt.agent.backend.db.ElasticsearchController import ESController
 
 '''
 SAMPLE FILE TO STORE REGISTRY KEYS
@@ -8,21 +9,9 @@ FOR NOW WE'LL USE FILE INSTEAD OF DB
 
 
 def search_registry(key):
-	'''
-	Searches the registry database (for now file) and returns if key is present
-	
-	key(str): key_name
-	'''
-	
-	storage_fname = 'all_keys.txt'
-	search_regex = '[^\\]+'
-	
-	# Will be replaced with function call to refer DB
-	# Reads through storage_fname and tells if key exists
-	with open(storage_fname) as fp:
-		registry = fp.read()
-		a = re.findall(r".*{}$".format(key), registry, flags = re.MULTILINE)
-		return len(a) != 0
+    es_handle = ESController()
+    if es_handle.getItem(key) > 0:
+        return True
 
 if __name__ == '__main__':
 	print(search_registry('Elements'))
