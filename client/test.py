8229069475692af985b9c614121063e91a0f75fb
Rittwik Adhikari <rittwik.adhikari@gmail.com>
2018-06-22 13:55:57 +0530

    patch to display only strings

--- client/test.py
+++ client/test.py
@@ -1,11 +1,14 @@
 from socketIO_client import SocketIO
 def message_response(data):
-    print(data)
+	if type(data) == type('a'):
+		print(data)
+	
 #Write the addition code from here
 
+
 #To here
-socketIO = SocketIO('localhost', 3000)
-mess = 'Hi praneeth'
+socketIO = SocketIO('10.33.34.19', 3000)
+mess = 'Example Message'
 # Listen
 
 socketIO.emit('message',mess)
@@ -13,4 +16,4 @@ socketIO.emit('message',mess)
 #socketIO.wait(seconds=7)
 socketIO.on('message', message_response)
 
-socketIO.wait()
+# socketIO.wait()
