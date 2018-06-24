0d19329f98b9648df3e7eca8adc0bdf9c0a07682
unknown <srisannidhi.mujumdar@FireEye.com>
2018-06-24 19:56:12 +0000

    Implemnted DB operations and calls for 'process' Search-type

--- client/pro-malhunt/com/malhunt/agent/backend/db/ElasticsearchController.py
+++ client/pro-malhunt/com/malhunt/agent/backend/db/ElasticsearchController.py
@@ -11,10 +11,25 @@ class ESController(object):
         self.config = configparser.ConfigParser()
         self.config.readfp(open(os.path.dirname(__file__) + '/../config/mhprop.ini'))
 
-    def insertItem(self, filename):
-        item = SearchItem(id = filename, name = os.path.basename(filename), type = 'file', ingestion_date = datetime.datetime.today(), md5 = None)
-        res = self.es_client.index(index=self.config['DEFAULT']['index'], doc_type=self.config['DEFAULT']['type'], id=item.id, body=vars(item))
-        return  res
+    def insertItem(self, name, type, id=None):
+        if type=='file':
+            id=name
+            name=os.path.basename(name)
+        elif type=='proc':
+            pass
+        item = SearchItem(id=id, name=name, type=type,
+                          ingestion_date=datetime.datetime.today(), md5=None)
+        return self.es_client.index(index=self.config['DEFAULT']['index'], doc_type=self.config['DEFAULT']['type'], id=item.id, body=vars(item))
+
+    #TODO: Note that 'name' param here for files refers to the whole path (The full qualified file name)
+    def purgeItem(self, name, type, id=None):
+        if type=='file':
+            id = name  #The full qualified name is being used as id for files
+        elif type=='proc':
+            id = id
+
+        return self.es_client.delete(index=self.config['DEFAULT']['index'], doc_type=self.config['DEFAULT']['type'], id=id)
+
 
     def getItem(self, name):
         res = self.es_client.search(index=self.config['DEFAULT']['index'], body = {'query':{'match':{'name':"'"+name+"'"}}})['hits']['total']
--- client/pro-malhunt/com/malhunt/agent/backend/test/test.py
+++ client/pro-malhunt/com/malhunt/agent/backend/test/test.py
@@ -4,6 +4,6 @@ from com.malhunt.agent.backend.db.ElasticsearchController import ESController
 es_handle = ESController()
 with open('C:\Users\qauser\Downloads\wfile.txt') as fin:
     for line in fin:
-        print es_handle.insertItem(line.strip())
+        print es_handle.insertItem(line.strip(), 'file')
 # if es_handle.getItem('mso30win32client.dll') >0:
 #     print('FOUND!!!')
