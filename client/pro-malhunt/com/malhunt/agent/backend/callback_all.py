44274ffebf9a5c6cf1172a7ce2b4ebcf8875f368
Rittwik Adhikari <rittwik.adhikari@gmail.com>
2018-06-26 14:43:20 +0530

    update functionality added

---
+++ client/pro-malhunt/com/malhunt/agent/backend/callback_all.py
@@ -0,0 +1,35 @@
+from file.file_api import UpdatedFilelist
+from process.proc_api import proc_api
+from threading import Thread
+
+class UpdateSys:
+	'''
+	Class to create threads for each updates of each process
+	
+	'''
+	
+	def __init__(self):
+		proc_upd_th = Thread(self._update_proc)
+		file_upd_th = Thread(self._update_file)
+		reg_upd_th = Thread(self._update_reg)
+		proc_upd_th.start()
+		file_upd_th.start()
+		reg_upd_th.start()
+		proc_upd_th.join()
+		file_upd_th.join()
+		reg_upd_th.join()
+	
+	def _update_file(self):
+		pass
+		
+	def _update_proc(self):
+		'''
+		Creates a threads for process updates
+		'''
+		
+		proc_obj = proc_api()
+		proc_obj.add_proc_callbacks()
+	
+	def _update_reg(self):
+		pass
+	
